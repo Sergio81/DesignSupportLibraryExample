851c668dde6c25bca8e7e27941823d03564d8afc
Sergio Fabian Aguilar Vega <sergiofav@outlook.com>
2018-10-11 09:29:03 -0400

    Implement SnackBar

--- app/src/main/java/com/androidtraining/designsupportlibraryexample/MainActivity.java
+++ app/src/main/java/com/androidtraining/designsupportlibraryexample/MainActivity.java
@@ -1,13 +1,29 @@
 package com.androidtraining.designsupportlibraryexample;
 
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NestedScrollView myNestedScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myNestedScrollView = findViewById(R.id.myNestedScrollView);
+
+
+    }
+
+    public void floatingActionButton_OnClick(View view) {
+        // Pass in the click listener when displaying the Snackbar
+        Snackbar.make(myNestedScrollView, "My snack text", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show(); // Don’t forget to show!
     }
 }
